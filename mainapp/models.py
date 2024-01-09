f48119c4a244cc87129c82ce1867488b0ebcae85
Леванов Роман Геннадьевич <10390@nordsy.spb.ru>
2024-01-09 14:20:29 +0300

    created models

---
+++ mainapp/models.py
@@ -0,0 +1,84 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Users(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Имя')
+    email = models.CharField(max_length=100, unique=True, verbose_name='Почта')
+    fam = models.CharField(max_length=50, verbose_name='Фамилия')
+    otc = models.CharField(max_length=50, verbose_name='Отчество')
+    phone = models.CharField(max_length=20, unique=True, verbose_name='Контактный телефон')
+
+    def __str__(self):
+        return self.name.username
+
+
+class Coordinates(models.Model):
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
+    height = models.IntegerField(verbose_name='Высота')
+
+    def __str__(self):
+        return f"широта: {self.latitude}, долгота: {self.longitude}, высота: {self.height}"
+
+    class Meta:
+        verbose_name = "Координаты"
+        verbose_name_plural = "Координаты"
+
+
+class Level(models.Model):
+    winter = models.CharField(max_length=2, verbose_name='Зима', blank=True)
+    summer = models.CharField(max_length=2, verbose_name='Лето', blank=True)
+    autumn = models.CharField(max_length=2, verbose_name='Осень', blank=True)
+    spring = models.CharField(max_length=2, verbose_name='Весна', blank=True)
+
+    def __str__(self):
+        return f"зима: {self.winter}, весна: {self.spring}, лето: {self.summer}, осень: {self.autumn}"
+
+    class Meta:
+        verbose_name = "Уровень сложности"
+        verbose_name_plural = "Уровни сложности"
+
+
+class Pereval(models.Model):
+    NEW = "New"
+    PENDING = "Pending"
+    ACCEPTED = "Accepted"
+    REJECTED = "Rejected"
+    STATUS_CHOICES = [
+        ("New", "новый"),
+        ("Pending", "в работе"),
+        ("Accepted", "принят"),
+        ("Rejected", "отклонен"),
+    ]
+
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, verbose_name='Пользователь')
+    coordinates = models.OneToOneField(Coordinates, on_delete=models.CASCADE, verbose_name='Координаты')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=NEW, verbose_name='Статус')
+    beauty_title = models.CharField(max_length=100, blank=True, verbose_name='Основное название вершины')
+    title = models.CharField(max_length=100, verbose_name='Название вершины')
+    other_titles = models.CharField(max_length=100, blank=True, verbose_name='Другое название' )
+    connect = models.CharField(max_length=100, blank=True, verbose_name='Связывает')
+    add_time = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.pk}: {self.beauty_title}"
+
+    class Meta:
+        verbose_name = "Перевал"
+        verbose_name_plural = "Перевалы"
+
+
+class Image(models.Model):
+    pereval = models.ForeignKey(Pereval, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
+    data = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name='Изображение',
+                             null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True, verbose_name="Название")
+    # date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.pk}: {self.title}"
+
+    class Meta:
+        verbose_name = "Изображение"
+        verbose_name_plural = "Изображения"
